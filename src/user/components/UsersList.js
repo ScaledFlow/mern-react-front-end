441fb5828ca57ecb8c77b9a3c53e85f76aee0e4f
John Leintz <johnleintz@scaledflow.com>
2020-12-15 17:57:35 -0600

    add get users with get request

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -4,11 +4,7 @@ import UserItem from './UserItem';
 import Card from '../../shared/components/UIElements/Card';
 import './UsersList.css';
 
-// var d = new Date();
-//console.log('userList.js ' + d.getSeconds() + " " + d.getMilliseconds());
-
 const UsersList = props => {
-  //console.log(props.items);
   if (props.items.length === 0) {
     return (
       <div className="center">
@@ -27,7 +23,7 @@ const UsersList = props => {
           id={user.id}
           image={user.image}
           name={user.name}
-          placeCount={user.places}
+          placeCount={user.places.length}
         />
       ))}
     </ul>
